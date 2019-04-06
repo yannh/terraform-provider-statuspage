0d4978a70e314eb22497592b784e99c3ec8f38e6
Yann Hamon <yann@mandragor.org>
2019-04-06 18:50:47 +0200

    more unit tests for SDK

--- vendor/github.com/yannh/statuspage-go-sdk/main.go
+++ vendor/github.com/yannh/statuspage-go-sdk/main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"time"
 )
@@ -16,6 +15,10 @@ const apiRoot = "https://api.statuspage.io/v1"
 // HTTPClient is the http wrapper for the application
 type HTTPClient interface {
 	Do(req *http.Request) (*http.Response, error)
+}
+
+type IClient interface {
+	doHTTPRequest(method, endpoint string, item interface{}) (resp *http.Response, err error)
 }
 
 type Client struct {
@@ -43,7 +46,6 @@ func (client *Client) doHTTPRequest(method, endpoint string, item interface{}) (
 
 	if item != nil {
 		data, err := json.Marshal(item)
-		log.Printf("Statuspage Data %s\n", data)
 		if err != nil {
 			return nil, err
 		}
@@ -70,7 +72,7 @@ func (client *Client) doHTTPRequest(method, endpoint string, item interface{}) (
 	return resp, err
 }
 
-func createResource(client *Client, pageID, resourceType string, resource, result interface{}) error {
+func createResource(client IClient, pageID, resourceType string, resource, result interface{}) error {
 	resp, err := client.doHTTPRequest(
 		"POST",
 		"/pages/"+pageID+"/"+resourceType+"s",
@@ -92,7 +94,7 @@ func createResource(client *Client, pageID, resourceType string, resource, resul
 	return fmt.Errorf("Failed creating %s, request returned %d", resourceType, resp.StatusCode)
 }
 
-func readResource(client *Client, pageID, ID, resourceType string, target interface{}) error {
+func readResource(client IClient, pageID, ID, resourceType string, target interface{}) error {
 	resp, err := client.doHTTPRequest(
 		"GET",
 		"/pages/"+pageID+"/"+resourceType+"s/"+ID,
@@ -113,7 +115,7 @@ func readResource(client *Client, pageID, ID, resourceType string, target interf
 	return fmt.Errorf("[ERROR] Statuspage could not find %s with ID: %s, http status %d", resourceType, ID, resp.StatusCode)
 }
 
-func updateResource(client *Client, pageID, resourceType, ID string, resource interface{}, result interface{}) error {
+func updateResource(client IClient, pageID, resourceType, ID string, resource interface{}, result interface{}) error {
 	resp, err := client.doHTTPRequest(
 		"PATCH",
 		"/pages/"+pageID+"/"+resourceType+"s/"+ID,
@@ -136,7 +138,7 @@ func updateResource(client *Client, pageID, resourceType, ID string, resource in
 	return fmt.Errorf("Failed updating %s, request returned %d", resourceType, resp.StatusCode)
 }
 
-func deleteResource(client *Client, pageID, resourceType, ID string) error {
+func deleteResource(client IClient, pageID, resourceType, ID string) error {
 	resp, err := client.doHTTPRequest(
 		"DELETE",
 		"/pages/"+pageID+"/"+resourceType+"s/"+ID,
--- vendor/github.com/yannh/statuspage-go-sdk/main_test.go
+++ vendor/github.com/yannh/statuspage-go-sdk/main_test.go
@@ -2,28 +2,60 @@ package statuspagesdk
 
 import (
 	"bytes"
+	"io/ioutil"
 	"net/http"
 	"strings"
 	"testing"
 )
 
 type mockHTTPClient struct {
-	req *http.Request
+	req     *http.Request
+	errCode int
+}
+
+func NewMockHTTPClient(errCode int) *mockHTTPClient {
+	return &mockHTTPClient{
+		errCode: errCode,
+	}
 }
 
 func (c *mockHTTPClient) Do(req *http.Request) (*http.Response, error) {
 	c.req = req
-	return nil, nil
+	return &http.Response{StatusCode: c.errCode}, nil
+}
+
+type mockClient struct {
+	resp *http.Response
+	err  error
+}
+
+func NewMockClient(resp *http.Response, err error) *mockClient {
+	return &mockClient{
+		resp: resp,
+		err:  err,
+	}
+}
+
+func (client *mockClient) doHTTPRequest(method, endpoint string, item interface{}) (resp *http.Response, err error) {
+	return client.resp, client.err
 }
 
 func TestDoHTTPRequest(t *testing.T) {
 	token := "token"
 	client := NewClient(token)
-	mc := &mockHTTPClient{}
+	mc := NewMockHTTPClient(200)
 	client.httpClient = mc
 
-	r1, _ := http.NewRequest("POST", "https://api.statuspage.io/v1/endpoint", strings.NewReader("{\"Name\":\"resource_name\"}"))
-	r2, _ := http.NewRequest("GET", "https://api.statuspage.io/v1/components", strings.NewReader("{\"Name\":\"resource_name\",\"Description\":\"description\"}"))
+	r1, _ := http.NewRequest(
+		"POST",
+		"https://api.statuspage.io/v1/endpoint",
+		strings.NewReader("{\"Name\":\"resource_name\"}"),
+	)
+	r2, _ := http.NewRequest(
+		"GET",
+		"https://api.statuspage.io/v1/components",
+		strings.NewReader("{\"Name\":\"resource_name\",\"Description\":\"description\"}"),
+	)
 
 	testCases := []struct {
 		method    string
@@ -75,5 +107,22 @@ func TestDoHTTPRequest(t *testing.T) {
 		if bodyWantS != bodyHaveS {
 			t.Errorf("Request Body should be %s, got %s", bodyWantS, bodyHaveS)
 		}
+	}
+}
+
+func TestCreateResource(t *testing.T) {
+	nameShouldBe := "resource name"
+	r := &http.Response{StatusCode: http.StatusCreated, Body: ioutil.NopCloser(bytes.NewReader([]byte("{\"Name\":\"" + nameShouldBe + "\"}")))}
+	client := NewMockClient(r, nil)
+
+	target := struct{ Name string }{}
+	err := createResource(client, "somePageID", "component", struct{ Name string }{nameShouldBe}, &target)
+
+	if err != nil {
+		t.Errorf("Error while creating resource: %+v", err)
+	}
+
+	if target.Name != nameShouldBe {
+		t.Errorf("Failed to read resources: %s, %s", nameShouldBe, target.Name)
 	}
 }
