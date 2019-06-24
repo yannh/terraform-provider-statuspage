85aae05a273d7bd52f4c3dc143679809b6c1f754
Yann Hamon <yann@mandragor.org>
2019-06-24 23:47:20 +0200

    indent, not parameterizing datadog metricProvider test

--- statuspage/resource_metric_provider_test.go
+++ statuspage/resource_metric_provider_test.go
@@ -5,18 +5,16 @@ import (
 	"os"
 	"testing"
 
-	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
 func TestAccStatuspageMetricProvider_Basic(t *testing.T) {
-	rid := acctest.RandIntRange(1000, 9999)
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccMetricProvider_basic(rid),
+				Config: testAccMetricProvider_basic(),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet("statuspage_metrics_provider.datadog", "id"),
 					resource.TestCheckResourceAttr("statuspage_metrics_provider.datadog", "type", "Datadog"),
@@ -26,7 +24,7 @@ func TestAccStatuspageMetricProvider_Basic(t *testing.T) {
 	})
 }
 
-func testAccMetricProvider_basic(rand int) string {
+func testAccMetricProvider_basic() string {
 	return fmt.Sprintf(`
 	variable "pageid" {
 		default = "%s"
@@ -42,9 +40,9 @@ func testAccMetricProvider_basic(rand int) string {
 
 	resource "statuspage_metrics_provider" "datadog" {
 		page_id = "${var.pageid}"
-	    api_key = "${var.api_key}"
-	    application_key = "${var.application_key}"
-	    type = "Datadog"
+		api_key = "${var.api_key}"
+		application_key = "${var.application_key}"
+		type = "Datadog"
 	}
 	`, pageID, os.Getenv("DATADOG_API_KEY"), os.Getenv("DATADOG_APPLICATION_KEY"))
 }
