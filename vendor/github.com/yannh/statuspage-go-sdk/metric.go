fd48c1402d906b5b0f331caf10eddcd53114fe7c
Yann Hamon <yann.hamon@contentful.com>
2020-02-20 21:34:42 +0100

    fix conflicts

--- vendor/github.com/yannh/statuspage-go-sdk/metric.go
+++ vendor/github.com/yannh/statuspage-go-sdk/metric.go
@@ -1,7 +1,6 @@
 package statuspage
 
 type Metric struct {
-<<<<<<< HEAD
 	Name               *string  `json:"name"`
 	MetricIdentifier   *string  `json:"metric_identifier,omitempty"`
 	Transform          *string  `json:"transform,omitempty"`
@@ -12,23 +11,10 @@ type Metric struct {
 	Display            *bool    `json:"display,omitempty"`
 	DecimalPlaces      *int     `json:"decimal_places,omitempty"`
 	TooltipDescription *string  `json:"tooltip_description,omitempty"`
-=======
-	Name               string  `json:"name"`
-	MetricIdentifier   string  `json:"metric_identifier,omitempty"`
-	Transform          string  `json:"transform,omitempty"`
-	Suffix             string  `json:"suffix,omitempty"`
-	YAxisMin           float64 `json:"y_axis_min,omitempty"`
-	YAxisMax           float64 `json:"y_axis_max,omitempty"`
-	YAxisHidden        bool    `json:"y_axis_hidden,omitempty"`
-	Display            bool    `json:"display,omitempty"`
-	DecimalPlaces      int     `json:"decimal_places,omitempty"`
-	TooltipDescription string  `json:"tooltip_description,omitempty"`
->>>>>>> dda28c7436493399fdfd1b1aed0ce0f1345df307
 }
 
 type MetricFull struct {
 	Metric
-<<<<<<< HEAD
 	ID                *string `json:"id"`
 	MetricsProviderID *string `json:"metrics_provider_id"`
 	MetricsDisplayID  *string `json:"metrics_display_id"`
@@ -36,15 +22,6 @@ type MetricFull struct {
 	MostRecentDataAt  *string `json:"most_recent_data_at"`
 	CreatedAt         *string `json:"created_at"`
 	UpdatedAt         *string `json:"updated_at"`
-=======
-	ID                string `json:"id"`
-	MetricsProviderID string `json:"metrics_provider_id"`
-	MetricsDisplayID  string `json:"metrics_display_id"`
-	Backfilled        bool   `json:"backfilled"`
-	MostRecentDataAt  string `json:"most_recent_data_at"`
-	CreatedAt         string `json:"created_at"`
-	UpdatedAt         string `json:"updated_at"`
->>>>>>> dda28c7436493399fdfd1b1aed0ce0f1345df307
 }
 
 func CreateMetric(client *Client, pageID, metricsProviderID string, metric *Metric) (*MetricFull, error) {
