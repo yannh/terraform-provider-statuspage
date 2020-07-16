06512bbe8461790e9f090e19320fe22a345aa34c
Yann Hamon <yannh@users.noreply.github.com>
2020-07-16 22:59:29 +0200

    Revert "add support for importing resources"

--- statuspage/resource_component_group_test.go
+++ statuspage/resource_component_group_test.go
@@ -34,32 +34,6 @@ func TestAccStatuspageComponentGroupBasic(t *testing.T) {
 	})
 }
 
-func TestAccStatuspageComponentGroup_BasicImport(t *testing.T) {
-	rid := acctest.RandIntRange(1000, 9999)
-
-	pageID = fmt.Sprintf("page-id-%d", rid)
-	componentGroupID := rid
-
-	resourceName := "statuspage_component_group.default"
-	importInput := fmt.Sprintf("%s/%d", pageID, componentGroupID)
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccComponentGroupBasic(rid),
-			},
-			{
-				ResourceName:      resourceName,
-				ImportStateId:     importInput,
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-		},
-	})
-}
-
 func testAccComponentGroupBasic(rand int) string {
 	return fmt.Sprintf(`
 	variable "component_name" {
