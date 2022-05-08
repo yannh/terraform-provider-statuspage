c8a8f34bfaa082cc8c6f122a368a79e883ad4b93
Yann Hamon <yann.hamon@contentful.com>
2022-05-08 14:17:27 +0200

    Upgrade SDK

--- statuspage/resource_component.go
+++ statuspage/resource_component.go
@@ -5,8 +5,8 @@ import (
 	"log"
 	"strings"
 
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/helper/validation"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	sp "github.com/yannh/statuspage-go-sdk"
 )
 
@@ -186,7 +186,7 @@ func resourceComponent() *schema.Resource {
 				Default:     "",
 				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
 					if new == "" {
-					  return true
+						return true
 					}
 					return false
 				},
